0da887be3f80ebffe54dd07fac0f8cd51f09f1ce
walle <nikitapitalenko1996@gmail.com>
2020-01-16 20:25:14 +0300

    Classes

---
+++ class.js
@@ -0,0 +1,8 @@
+class Beer {
+    constructor(opt) {
+         this.name = opt.name; 
+        this.tagline = opt.tagline;
+        this.first_brewed = opt.first_brewed;
+        this.description = opt.description;
+    
+        }
