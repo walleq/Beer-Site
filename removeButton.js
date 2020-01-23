2908ab198f0e3943a2e37993c59f18ef8b4cab18
walle <nikitapitalenko1996@gmail.com>
2020-01-23 19:09:20 +0300

    Delete Button

---
+++ removeButton.js
@@ -0,0 +1,7 @@
+const removeButton = document.getElementsById('elem');
+removeButton.addEventListener('click', function(target) {
+    console.log('Куку, епта')
+}) ;
+
+
+/* Отлавливать клик только на кнопку через таргет по класс нейму */
